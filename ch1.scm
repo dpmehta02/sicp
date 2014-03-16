86cfa14fa0aefa81b1506932d48767e6c7b9f537
dpmehta02 <dpmehta02@gmail.com>
2014-03-16 12:10:56 -0700

    complete ex 1.3

--- ch1.scm
+++ ch1.scm
@@ -1,6 +1,33 @@
+; Ex 1.3
+; My solution
 (define (sum-of-squares a b c)
-  (cond (and (< a b) (< a c)) (+ (* b b) (* c c))
-        (and (< a b) (< a c)) (+ (* b b) (* c c))
-        (                     (+ (* b b) (* c c))
+  (cond ((and (<= a b) (<= a c)) (+ (* b b) (* c c)))
+        ((and (<= b a) (<= b c)) (+ (* a a) (* c c)))
+                                 (+ (* a a) (* b b))
   )
 )
+
+; Community solution
+(define  
+     (largest-two-square-sum x y z)  
+         (if (= x (larger x y))  
+             (sum-of-squares x (larger y z))  
+             (sum-of-squares y (larger x z)) 
+         ) 
+ ) 
+  
+ (define  
+     (larger x y)  
+         (if (> x y) x y) 
+ ) 
+  
+ (define  
+     (sum-of-squares x y)  
+         (+ (square x) (square y)) 
+ ) 
+  
+ (define  
+     (square x)  
+         (* x x) 
+ ) 
+  
