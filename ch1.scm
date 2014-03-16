1910e9ff025f8db7b6b386455e1fdba802d323c6
dpmehta02 <dpmehta02@gmail.com>
2014-03-16 12:29:48 -0700

    ex 1.4

--- ch1.scm
+++ ch1.scm
@@ -1,33 +1,25 @@
-; Ex 1.3
-; My solution
-(define (sum-of-squares a b c)
-  (cond ((and (<= a b) (<= a c)) (+ (* b b) (* c c)))
-        ((and (<= b a) (<= b c)) (+ (* a a) (* c c)))
-                                 (+ (* a a) (* b b))
-  )
-)
+; Ex 1.3: Define a procedure that takes three numbers as arguments and returns
+; the sum of the squares of the two larger numbers. 
+  
+(define (square x) (* x x)) 
 
-; Community solution
-(define  
-     (largest-two-square-sum x y z)  
-         (if (= x (larger x y))  
-             (sum-of-squares x (larger y z))  
-             (sum-of-squares y (larger x z)) 
-         ) 
- ) 
+(define (sum-of-squares x y) (+ (square x) (square y))) 
+
+(define (sum-of-squared-largest-two x y z) 
+        (cond ((= (min x y z) x) (sum-of-squares y z))
+              ((= (min x y z) y) (sum-of-squares x z))
+              ((= (min x y z) z) (sum-of-squares x y))))
   
- (define  
-     (larger x y)  
-         (if (> x y) x y) 
- ) 
-  
- (define  
-     (sum-of-squares x y)  
-         (+ (square x) (square y)) 
- ) 
-  
- (define  
-     (square x)  
-         (* x x) 
- ) 
-  
+ ;; Testing
+(newline)
+(display (sum-of-squared-largest-two 1 3 4)) (newline)
+(display (sum-of-squared-largest-two 4 1 3)) (newline)
+(display (sum-of-squared-largest-two 3 4 1)) (newline)
+
+; Ex 1.4
+; Defines a function that takes two inputs and adds the first to the absolute
+; value of the second with the following trick: if b is positive, the function
+; adds a and b; if b is negative, the function subtracts b from a, in effect 
+; adding them together
+(define (a-plus-abs-b a b)
+  ((if (> b 0) + -) a b))
