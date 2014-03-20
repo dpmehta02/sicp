6d4ec73e31309caaeda2691a5010d15fe250fbed
dpmehta02 <dpmehta02@gmail.com>
2014-03-19 20:31:48 -0700

    1.11

--- ch1.scm
+++ ch1.scm
@@ -188,3 +188,26 @@
 
 ; Give concise mathematical definitions for the functions computed by the procedures f, g, and h for positive integer values of n. For example, (k n) computes 5n2.
 
+; Ex 1.11.  A function f is defined by the rule that
+;f(n) = n if n<3 and f(n) = f(n - 1) + 2f(n - 2) + 3f(n - 3)
+; if n> 3. Write a procedure that computes f by means of a 
+; recursive process. Write a procedure that computes f by 
+; means of an iterative process.
+
+; Recursive
+(define (f n) 
+  (cond ((< n 3) n) 
+       (else (+ (f (- n 1))
+                (* 2 (f (- n 2))) 
+                (* 3 (f (- n 3)))))))
+
+; Iterative
+(define (foo n) 
+  (define (foo-iter a b c n) 
+    (if (< n 3) 
+      a 
+      (foo-iter (+ a (* 2 b) (* 3 c)) a b (- n 1)))) 
+  (if (< n 3) 
+    n 
+    (foo-iter 2 1 0 n))) 
+
