16266d87d5bef6c93bf5628a4b5ec380b9363104
dpmehta02 <dpmehta02@gmail.com>
2014-03-25 05:20:06 -0700

    1.20

--- ch1.scm
+++ ch1.scm
@@ -268,4 +268,126 @@
                        q 
                        (- count 1))))) 
 
- (define (square x) (* x x)) 
+ (define (square x) (* x x))
+
+; Exercise 1.20.  The process that a procedure generates is of course dependent
+; on the rules used by the interpreter. As an example, consider the iterative
+; gcd procedure given above. Suppose we were to interpret this procedure using
+; normal-order evaluation, as discussed in section 1.1.5. (The normal-order-
+; evaluation rule for if is described in exercise 1.5.) Using the substitution
+; method (for normal order), illustrate the process generated in evaluating (gcd
+; 206 40) and indicate the remainder operations that are actually performed. How
+; many remainder operations are actually performed in the normal-order
+; evaluation of (gcd 206 40)? In the applicative-order evaluation?
+
+; (define (gcd a b)
+;   (if (= b 0)
+;       a
+;       (gcd b (remainder a b))))
+
+; Normal Order (http://www.billthelizard.com/2010/01/sicp-exercise-120-gcd.html)
+(gcd 206 40)
+
+(if (= 40 0)
+    206
+    (gcd 40 (remainder 206 40)))
+
+(gcd 40 (remainder 206 40))
+
+(if (= (remainder 206 40) 0)
+    40
+    (gcd (remainder 206 40) (remainder 40 (remainder 206 40))))
+
+(if (= 6 0) ;; 1 remainder evaluated
+    40
+    (gcd (remainder 206 40) (remainder 40 (remainder 206 40))))
+
+(gcd (remainder 206 40) (remainder 40 (remainder 206 40)))
+
+(if (= (remainder 40 (remainder 206 40)) 0)
+    (remainder 206 40)
+    (gcd (remainder 40 (remainder 206 40))
+         (remainder (remainder 206 40)
+                    (remainder 40 (remainder 206 40)))))
+
+(if (= 4 0) ;; 2 remainders evaluated
+    (remainder 206 40)
+    (gcd (remainder 40 (remainder 206 40))
+         (remainder (remainder 206 40)
+                    (remainder 40 (remainder 206 40)))))
+
+(gcd (remainder 40 (remainder 206 40))
+     (remainder (remainder 206 40)
+                (remainder 40 (remainder 206 40))))
+
+(if (= (remainder (remainder 206 40)
+                  (remainder 40 (remainder 206 40)))
+       0)
+       (remainder 40 (remainder 206 40))
+       (gcd (remainder (remainder 206 40)
+                       (remainder 40 (remainder 206 40)))
+            (remainder (remainder 40 (remainder 206 40))
+                       (remainder (remainder 206 40)
+                                  (remainder 40 (remainder 206 40))))))
+
+(if (= 2 0) ;; 4 remainders evaluated (THIS IS SO CONFUSING)
+    (remainder 40 (remainder 206 40))
+    (gcd (remainder (remainder 206 40)
+                    (remainder 40 (remainder 206 40)))
+         (remainder (remainder 40 (remainder 206 40))
+                    (remainder (remainder 206 40)
+                               (remainder 40 (remainder 206 40))))))
+
+(gcd (remainder (remainder 206 40)
+                (remainder 40 (remainder 206 40)))
+     (remainder (remainder 40 (remainder 206 40))
+                (remainder (remainder 206 40)
+                           (remainder 40 (remainder 206 40)))))
+
+(if (= (remainder (remainder 40 (remainder 206 40))
+                  (remainder (remainder 206 40)
+                             (remainder 40 (remainder 206 40)))) 0)
+    (remainder (remainder 206 40)
+               (remainder 40 (remainder 206 40)))
+    (gcd (remainder (remainder 40 (remainder 206 40))
+                    (remainder (remainder 206 40)
+                               (remainder 40 (remainder 206 40))))
+         (remainder (remainder (remainder 206 40)
+                               (remainder 40 (remainder 206 40)))
+                    (remainder (remainder 40 (remainder 206 40))
+                               (remainder (remainder 206 40)
+                                          (remainder 40
+                                                     (remainder 206 40)))))))
+
+(if (= 0 0) ;; 7 remainders evaluated
+    (remainder (remainder 206 40)
+               (remainder 40 (remainder 206 40)))
+    (gcd (remainder (remainder 40 (remainder 206 40))
+                    (remainder (remainder 206 40)
+                               (remainder 40 (remainder 206 40))))
+         (remainder (remainder (remainder 206 40)
+                               (remainder 40 (remainder 206 40)))
+                    (remainder (remainder 40 (remainder 206 40))
+                               (remainder (remainder 206 40)
+                                          (remainder 40
+                                                     (remainder 206
+                                                                40)))))))
+
+(remainder (remainder 206 40)
+           (remainder 40 (remainder 206 40)))
+
+; 18 remainders performed
+
+; Applicative
+; (gcd 206 40)
+; (gcd 40 (remainder 206 40))
+; (gcd 40 6)
+; (gcd 6 (remainder 40 6))
+; (gcd 6 4)
+; (gcd 4 (remainder 6 4))
+; (gcd 4 2)
+; (gcd 2 (remainder 4 2))
+; (gcd 2 0)
+; 2
+
+; 4 remainder operations performed
